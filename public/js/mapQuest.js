9e6b4bf4778a34923f577a49b33928b85fcaef26
AllysonLlacuna <allacuna@ucsd.edu>
2019-02-28 13:37:27 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'DqCGUjEhsaWxKB8TWwRjK1GxW3oofJ7a';
+
+	var map = L.mapquest.map('map',{
+		center:[32.879029, -117.235901],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+
+	});
+
+	L.marker([32.879029, -117.235901]).addTo(map);
 }
